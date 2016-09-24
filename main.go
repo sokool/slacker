7f70c86a5aac5c9153155bbbce052d66b76ff7c6
Michał Sokołowski <michal.sokolowski@smtsoftware.com>
2016-09-24 13:03:57 +0200

    slacker boot server implementation

--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+	"log"
+
+	"fmt"
+
+	"github.com/sokool/slacker/server"
+)
+
+type WelcomeResponse struct {
+	Text string `json:"text"`
+}
+
 func main() {
 
+	server.Register(Welcome)
+
+	if err := server.Run(); err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func Welcome(m server.Message) interface{} {
+	return WelcomeResponse{fmt.Sprintf("Eloszki, %s !", m.UserName)}
 }
